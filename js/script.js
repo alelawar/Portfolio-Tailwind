37f5792ded6931780fc01951a964f249334fe28a
Ahmad Lesmana <ahmadlesmana788@gmail.com>
2025-04-05 19:53:12 +0700

    responsive

--- js/script.js
+++ js/script.js
@@ -9,3 +9,25 @@ function nextSlide() {
 }
 
 setInterval(nextSlide, 3000); // Ganti gambar setiap 3 detik
+
+
+const menu = document.getElementById('menu');
+const navMobile = document.getElementById('nav-mobile');
+
+menu.addEventListener('click', function (e) {
+  e.stopPropagation(); // biar gak langsung kena event window
+  menu.classList.toggle('active-menu');
+  navMobile.classList.toggle('hidden');
+});
+
+navMobile.addEventListener('click', function (e) {
+  e.stopPropagation(); // biar gak nutup nav saat kita klik isi nav-nya
+});
+
+window.addEventListener('click', function () {
+  if (!navMobile.classList.contains('hidden')) {
+    // kalau nav-nya lagi aktif (tidak hidden), kita tutup
+    navMobile.classList.add('hidden');
+    menu.classList.remove('active-menu');
+  }
+});
